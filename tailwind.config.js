73c454310cf53b0827d6007943d9f0212121f9ec
Henry Bley-Vroman <olets@olets.dev>
2023-02-02 08:10:25 -1000

    refactor(styles): move all styles into tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,4 +34,36 @@ module.exports = {
       }
     }
   },
+  plugins: [
+    ({ addBase, addComponents }) => {
+      addBase({
+        a: {
+          '@apply decoration-orange-300 transition-colors underline focus:decoration-orange-500 focus:text-orange-500 hover:decoration-orange-500 hover:text-orange-500': {},
+        },
+        em: {
+          '@apply font-normal font-serif': {},
+        },
+        '@font-face': {
+          fontFamily: 'Modak',
+          src: 'url(Modak-Regular-subset.woff2) format("woff2")',
+        },
+        '@font-face': {
+          fontFamily: 'UnifrakturCook',
+          src: 'url(UnifrakturCook-Bold-subset.woff2) format("woff2")',
+        },
+      })
+
+      addComponents({
+        '.groundhog': {
+          '@apply absolute bg-center bg-contain bg-groundhog inset-10 sm:inset-12 md:inset-20 lg:inset-24 xl:inset-32 p-5 sm:p-10 md:p-20 lg:p-32 xl:p-40': {},
+        },
+        '.part': {
+          '@apply bg-black/70 leading-relaxed relative rounded-lg text-white': {},
+        },
+        '.spacing': {
+          '@apply lg:space-y-20 md:space-y-16 space-y-10': {},
+        },
+      })
+    }
+  ]
 }
